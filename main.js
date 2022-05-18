087e1ff30bb464ae1f2f8bba4f1b4b80a6b5fc2a
anushka5678 <78417553+anushka5678@users.noreply.github.com>
2022-05-18 15:47:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -16,9 +21,23 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
     }
 } 
 
 function draw(){
     background('#eb81b8');
+
+    document.getElementById("square_side").innerHTML = "Width and Height of the Square will be = " + difference + "px";
+    fill('#3fc0fc');
+    stroke('#3fc0fc');
+    square(noseX, noseY, difference);
 }
